967b54fa4168690c6d7d507d96397f6721aa433f
grapefrukt <grapefrukt@grapefrukt.com>
2013-07-13 17:37:12 +0200

    Helper functions for email and callto links Also made it return instead of echo

--- helpers/viewhelper.php
+++ helpers/viewhelper.php
@@ -8,8 +8,17 @@ class ViewHelper {
 		include 'views/' . $name . '.php';
 	}
 
-	public function link($url){
-		echo '<a href="', $url, '">', $url, '</a>';
+	public function link($url, $text = ''){
+		if($text == '') $text = $url;
+		return '<a href="' . $url . '">' . $text . '</a>';
+	}
+
+	public function email($email, $text = ''){
+		return $this->link('mailto:' . $email, $text == '' ? $email : $text);
+	}
+
+	public function callto($phone, $text = ''){
+		return $this->link('callto:' . urlencode($phone), $text == '' ? $phone : $text);
 	}
 }
 
