a28ae3444722a22499dc6550782c6ceea2110590
grapefrukt <grapefrukt@grapefrukt.com>
2013-07-14 21:55:17 +0200

    Added config file to set up base path

---
+++ config-sample.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+* The install path on your server, if it's in the root ie. http://altpresskit.grapefrukt.com leave as is
+* If in a subfolder like http://grapefrukt.com/presskit/, put presskit/ below 
+*/
+define('BASE_PATH', '/');
+
+?>
--- index.php
+++ index.php
@@ -2,33 +2,25 @@
 ini_set('display_errors','On');
 error_reporting(-1);
 
+require 'config.php';
 require 'router/Router.php';
 require 'router/Route.php';
 require 'controllers/presskit.php';
 
 $router = new Router();
 
-$router->setBasePath('');
+$router->setBasePath(BASE_PATH);
 
+$router->map(':id', array('controller' => 'PresskitController', 'action' => 'game'));
 $router->map('/', array('controller' => 'PresskitController', 'action' => 'index'));
-$router->map('/:id', array('controller' => 'PresskitController', 'action' => 'game'));
 
 $route = $router->matchCurrentRequest();
 
 ob_start();
 
 if($route) { 
-	/*?>
-	<strong>Target:</strong>
-	<pre><?php var_dump($route->getTarget()); ?></pre>
-
-	<strong>Parameters:</strong>
-	<pre><?php var_dump($route->getParameters()); ?></pre>
-
-	<?php */
 	$target = $route->getTarget();
 	$controller = new $target["controller"]();
-	
 	
 	call_user_func_array(array($controller, $target['action']), $route->getParameters());
 
