e083a02e66ed4bda6a64eb3336369735e254685a
grapefrukt <grapefrukt@grapefrukt.com>
2013-07-14 20:54:40 +0200

    Added missing dl tags to additionals list

--- views/additionals.php
+++ views/additionals.php
@@ -1,10 +1,11 @@
 <div id="additionals" class="sixteen columns">
 	<h2>Additional Links</h2>
-	
-	<?php foreach($data->additionals as $additional): ?>
-		<dt><?php echo $additional['title']; ?></dt>
-		<dd><?php echo $additional['description'], ' ', ViewHelper::link($additional['link']); ?></dd>
-	<?php endforeach; ?>
+	<dl>
+		<?php foreach($data->additionals as $additional): ?>
+			<dt><?php echo $additional['title']; ?></dt>
+			<dd><?php echo $additional['description'], ' ', ViewHelper::link($additional['link']); ?></dd>
+		<?php endforeach; ?>
+	</dl>
 </div>
 
 <hr class="sixteen columns" />
