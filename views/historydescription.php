0849c83f4b03919a2f207634e33af6fde4d17955
grapefrukt <grapefrukt@grapefrukt.com>
2013-07-15 13:33:11 +0200

    Fixes issue where last element in "top" area would align incorrectly due to short fact sheet

--- views/historydescription.php
+++ views/historydescription.php
@@ -1,40 +1,41 @@
-<div id="description" class="eight columns">
-	<h2>Description</h2>
-	<p><?php echo $data->description; ?></p>
-</div>
-
-<?php if (isset($data->histories)): ?>
-<div id="history" class="eight columns">
-	<h2>History</h2>
-	<?php 
-	foreach($data->histories as $history){
-		if(isset($history['header'])) echo '<h3>', $history['header'], '</h3>';
-		echo '<p>', $history['text'], '</p>';
-	}
-	?>
-</div>
-<?php endif; ?>
-
-<?php if(isset($data->features)): ?>
-	<div id="features" class="eight columns">
-		<h2>Features</h2>
-		<ul class="square">
-		<?php foreach($data->features as $feature){
-			echo '<li>', $feature, '</li>';
-		} ?>
-		</ul>
+<div class="eight columns alpha omega">
+	<div id="description" class="eight columns">
+		<h2>Description</h2>
+		<p><?php echo $data->description; ?></p>
 	</div>
-<?php endif; ?>
 
-<?php if(isset($data->games)): ?>
-	<div id="games" class="eight columns">
-		<h2>Projects</h2>
-		<ul class="square">
-		<?php foreach($data->games as $game){
-			echo '<li>', ViewHelper::link($game->directory, $game->title), '</li>';
-		} ?>
-		</ul>
+	<?php if (isset($data->histories)): ?>
+	<div id="history" class="eight columns">
+		<h2>History</h2>
+		<?php 
+		foreach($data->histories as $history){
+			if(isset($history['header'])) echo '<h3>', $history['header'], '</h3>';
+			echo '<p>', $history['text'], '</p>';
+		}
+		?>
 	</div>
-<?php endif; ?>
+	<?php endif; ?>
 
+	<?php if(isset($data->features)): ?>
+		<div id="features" class="eight columns">
+			<h2>Features</h2>
+			<ul class="square">
+			<?php foreach($data->features as $feature){
+				echo '<li>', $feature, '</li>';
+			} ?>
+			</ul>
+		</div>
+	<?php endif; ?>
+
+	<?php if(isset($data->games)): ?>
+		<div id="games" class="eight columns">
+			<h2>Projects</h2>
+			<ul class="square">
+			<?php foreach($data->games as $game){
+				echo '<li>', ViewHelper::link($game->directory, $game->title), '</li>';
+			} ?>
+			</ul>
+		</div>
+	<?php endif; ?>
+</div>
 <hr class="twelve columns" />
