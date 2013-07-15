c75435c7d8123062f5de2ea17706d44658145c17
grapefrukt <grapefrukt@grapefrukt.com>
2013-07-15 13:43:30 +0200

    Corrected base path to be completely empty as default, a single slash breaks subfolder detection

--- config-sample.php
+++ config-sample.php
@@ -1,9 +1,9 @@
 <?php
 
 /**
-* The install path on your server, if it's in the root ie. http://altpresskit.grapefrukt.com leave as is
+* The install path on your server, if it's in the root ie. http://altpresskit.grapefrukt.com leave it empty
 * If in a subfolder like http://grapefrukt.com/presskit/, put presskit/ below 
 */
-define('BASE_PATH', '/');
+define('BASE_PATH', '');
 
 ?>
