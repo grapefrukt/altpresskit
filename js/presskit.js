52e199a8f2e1ac69f473a6b28956466d0db1350a
grapefrukt <grapefrukt@grapefrukt.com>
2013-07-16 23:54:48 +0200

    Styled menu, made it sticky, trying some color

--- js/presskit.js
+++ js/presskit.js
@@ -1,8 +1,28 @@
 $(document).ready(function(){
+
+	// populates menu using all h2 tags present on page
 	var menu = '<ul>';
 	$('h2').each(function() {
 		menu += '<li><a href="#' + $(this).parent().attr('id') + '">' + $(this).text() + '</a></li>'
 	});
 	menu += '</ul>';
 	$('#menu').html(menu);
+
+
+	// makes menu stick to top of screen
+	var dummymenu = $('#dummy-menu');
+	var menu = $('#menu');
+
+	var updateMenu = function() {
+		var menutop = dummymenu.offset().top - 10;
+		var scrolltop = $(window).scrollTop();
+		if(scrolltop < menutop && menu.css('position') == 'fixed' ){
+			menu.css('top', menutop - Math.max(scrolltop, 0));
+		} else {
+			menu.css('top', 0);
+		}
+	};
+
+	$.event.add(window, "scroll", updateMenu);
+	$.event.add(window, "resize", updateMenu);
 });
