e5b44f5b37f6d477b1be16a599a0dadf2e96ebeb
Martin Jonasson <grapefrukt@grapefrukt.com>
2013-10-01 16:27:10 +0200

    Split out loading code from promoter helper into own class, exposed various utility functions for checking cache and getting file names

---
+++ helpers/loadhelper.php
@@ -0,0 +1,99 @@
+<?php
+
+class LoadHelper {
+
+/*
+	simpleCachedCurl V1.1
+	Dirk Ginader
+	ginader.com
+
+	code: http://github.com/ginader
+
+	easy to use cURL wrapper with added file cache
+
+	usage: created a folder named "cache" in the same folder as this file and chmod it 777
+	call this function with 3 parameters:
+		$url (string) the URL of the data that you would like to load
+		$expires (integer) the amound of seconds the cache should stay valid
+		$debug (boolean, optional) write debug information for troubleshooting
+		
+	returns either the raw cURL data or false if request fails and no cache is available
+
+	*/
+	public static function loadCached($url, $expires, $debug = false){
+		if (!function_exists('curl_version')){
+			ErrorHelper::logWarning('Could not fetch data, cURL unavailable');
+			return null;
+		}
+		
+		$filename = LoadHelper::getFilename($url);
+		
+		if (!LoadHelper::hasCache($url, $expires)) {
+			if ($debug) ErrorHelper::logDebug('No cache or expired, making new request');
+
+			$rawData = LoadHelper::load($url);
+			
+			if (!$rawData) {
+				if ($debug) ErrorHelper::logDebug('cURL request failed:' . $error);
+				if ($changed != 0){
+					if($debug) ErrorHelper::logDebug('Using expired cache');
+					$cache = unserialize(file_get_contents($filename));
+					return $cache;
+				} else {
+					if ($debug) ErrorHelper::logDebug('Request failed, no cache');
+					return false;
+				}
+			}
+
+			if ($debug) ErrorHelper::logDebug('Data returned, saving it to cache');
+
+			$cache = fopen($filename, 'wb');
+			$write = fwrite($cache, serialize($rawData));
+
+			if ($debug && !$write) ErrorHelper::logDebug('Writing to ' . $filename . ' failed. Make sure the folder ' . dirname(__FILE__) . '/cache/ is writeable (chmod 777)');
+
+			fclose($cache);
+			return $rawData;
+		}
+
+		if ($debug) ErrorHelper::logDebug('Cache hit, using that');
+
+		return unserialize(file_get_contents($filename));
+	}
+
+	public static function hasCache($url, $expires){
+		$filename = LoadHelper::getFilename($url);
+		$changed = 0;
+
+		if (!file_exists($filename)) return false;
+
+		$changed = filemtime($filename);
+		$now = time();
+		$diff = $now - $changed;
+
+		return $diff < $expires;
+	}
+
+	public static function load($url){
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+		// we need to load custom https certificates here to be able to load from github securely
+		curl_setopt($ch, CURLOPT_CAINFO, dirname(__FILE__).'/cert/cacert.pem');
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1); 
+
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+
+		$rawData = curl_exec($ch);
+		$error = curl_error($ch);
+		curl_close($ch);
+
+		return $rawData;
+	}
+
+	public static function getFilename($url) {
+		return dirname(__FILE__).'/../cache/' . md5($url) . '.cache';
+	}
+}
+?>
--- helpers/promoterhelper.php
+++ helpers/promoterhelper.php
@@ -1,9 +1,11 @@
 <?php 
 
+require 'loadhelper.php';
+
 class PromoterHelper {
 
 	public static function getData($game){
-		$data = PromoterHelper::simpleCachedCurl('http://promoterapp.com/dopresskit/' . $game->promoter['product'], PROMOTER_CACHE_DURATION);
+		$data = LoadHelper::loadCached('http://promoterapp.com/dopresskit/' . $game->promoter['product'], PROMOTER_CACHE_DURATION);
 		if($data == null) return;
 
 		$promoterxml = simplexml_load_string($data);
@@ -21,85 +23,6 @@ class PromoterHelper {
 				'website' => 	 $review['publicationName'],
 			);
 		}
-	}
-
-	/*
-	simpleCachedCurl V1.1
-	Dirk Ginader
-	ginader.com
-
-	code: http://github.com/ginader
-
-	easy to use cURL wrapper with added file cache
-
-	usage: created a folder named "cache" in the same folder as this file and chmod it 777
-	call this function with 3 parameters:
-		$url (string) the URL of the data that you would like to load
-		$expires (integer) the amound of seconds the cache should stay valid
-		$debug (boolean, optional) write debug information for troubleshooting
-		
-	returns either the raw cURL data or false if request fails and no cache is available
-
-	*/
-	private static function simpleCachedCurl($url, $expires, $debug = false){
-		if (!function_exists('curl_version')){
-			ErrorHelper::logError("Could not fetch promoter data, cURL unavailable");
-			return null;
-		} 
-
-		if($debug){
-			echo "simpleCachedCurl debug:<br>";
-		}
-		$hash = md5($url);
-		$filename = dirname(__FILE__).'/../cache/' . $hash . '.cache';
-		$changed = 0;
-		if(file_exists($filename)) {
-		  $changed = filemtime($filename);
-		}
-		$now = time();
-		$diff = $now - $changed;   
-		if ( $changed == 0 || ($diff > $expires) ) {
-			if($debug){
-				echo "no cache or expired --> make new request<br>";
-			}
-			$ch = curl_init();
-			curl_setopt($ch, CURLOPT_URL, $url);
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-			$rawData = curl_exec($ch);
-			curl_close($ch);
-			if(!$rawData){
-				if($debug){
-					echo "cURL request failed<br>";
-				}
-				if($changed != 0){
-					if($debug){
-						echo "at least we have an expired cache --> better than nothing --> read it<br>";
-					}
-					$cache = unserialize(file_get_contents($filename));
-					return $cache;
-				}else{
-					if($debug){
-						echo "request failed and we have no cache at all --> FAIL<br>";
-					}
-					return false;
-				}
-			}
-			if($debug){
-				echo "we got a return --> save it to cache<br>";
-			}
-			$cache = fopen($filename, 'wb');
-			$write = fwrite($cache, serialize($rawData));
-			if($debug && !$write){
-				echo "writing to $filename failed. Make the folder '".dirname(__FILE__).'/cache/'."' is writeable (chmod 777)<br>";
-			}
-			fclose($cache);
-			return $rawData;
-		}
-		if($debug){
-			echo "yay we hit the cache --> read it<br>";
-		}
-		$cache = unserialize(file_get_contents($filename));
-		return $cache;
 	}
 }
 ?>
