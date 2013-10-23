38029433b6ad0d2569c90112803cef8baa056854
Martin Jonasson <grapefrukt@grapefrukt.com>
2013-10-24 00:18:16 +0200

    Bumped version

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 ini_set('display_errors','On');
 error_reporting(-1);
 
-define('VERSION', '0.0.3');
+define('VERSION', '0.0.4');
 
 require 'controllers/presskit.php';
 require 'helpers/errorhelper.php';
