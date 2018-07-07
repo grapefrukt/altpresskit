0ad57a0ca2cdc9b1fec01ff35a1e4f0b7a06808d
Martin Jonasson <grapefrukt@grapefrukt.com>
2018-07-07 09:15:26 +0200

    better blacklist, ignores all files/folders that start with periods or underscores

--- helpers/filehelper.php
+++ helpers/filehelper.php
@@ -2,44 +2,50 @@
 
 class FileHelper {
 
-	private static $blacklist = array('.', '..', 'data.xml', 'images', 'trailers', 'README', 'Thumbs.db', '.DS_Store');
+	private static $blacklist = array('data.xml', 'images', 'trailers', 'README', 'Thumbs.db');
 
 	public static function getGames($path){
 		if (!($handle = @opendir($path))) return null;
-		
+
 		$dirs = array();
 
 		while (false !== ($entry = readdir($handle))) {
 			if (array_search($entry, FileHelper::$blacklist) !== false) continue;
 			// folder starts with an underscore, ignore it
 			if (substr($entry, 0, 1) === "_") continue;
+			// folder starts with a period, ignore it
+			if (substr($entry, 0, 1) === ".") continue;
 			if ($entry == '') continue;
 
 			$dirs[] = $entry;
 		}
-		
+
 		closedir($handle);
-		
+
 		sort($dirs);
-		
+
 		return $dirs;
 	}
 
 	public static function getImages($path){
 		if (!($handle = @opendir($path))) return null;
-		
+
 		$images = array();
 
 		while (false !== ($entry = readdir($handle))) {
 			if (array_search($entry, FileHelper::$blacklist) !== false) continue;
 			if (!is_file($path . '/' . $entry)) continue;
+			// file starts with an underscore, ignore it
+			if (substr($entry, 0, 1) === "_") continue;
+			// file starts with a period, ignore it
+			if (substr($entry, 0, 1) === ".") continue;
 			$images[] = $path . '/' . $entry;
 		}
-		
+
 		closedir($handle);
 
 		sort($images);
-		
+
 		return $images;
 	}
 
